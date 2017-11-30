3923805688a14c1b62469835ec3d7acafd4a811c
Chris Aubert <hello@chrisaubert.me>
2017-11-30 21:11:02 +0000

    Vastly improved landing page

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,8 +3,9 @@
 import React, { Component }  from 'react'
 import cx from 'classnames'
 import moment from 'moment'
+import { Link } from 'react-router-dom'
 
-import { Image, Nav, CardContainer, Card, Button, WidthControlled } from '../ui'
+import { Image, Nav, CardContainer, Card, Button, WidthControlled, UPRacingLogo } from '../ui'
 import selector from '../../store/selector'
 
 import style from './style.scss'
@@ -18,6 +19,7 @@ type Props = {
 type State = {
 	activeImage: number,
 	fadeHero: boolean,
+	loaded: boolean,
 }
 
 class HomePage extends Component {
@@ -25,17 +27,45 @@ class HomePage extends Component {
 	props: Props
 
 	state: State = {
-		activeImage: 0,
-		fadeHero: false,
+		activeImage: Math.floor(Math.random() * this.props.store.homePage.heros.length),
+		fadeHero: true,
+		loaded: false,
 	}
 
 	componentDidMount = () => {
-		setInterval(this.changeImage, 5000)
+		setTimeout(() => {
+			this.setState({ fadeHero: false })
+			setInterval(this.changeImage, 10000)
+		}, 3500)
+		this.setState({ loaded: true })
 	}
 
 	render() {
 		const store = this.props.store
 		const posts = selector.findMostRecentBlogPosts(store, 4)
+
+		return (
+			<div className={style.container}>
+				<div className={style.logoContainer}>
+					<UPRacingLogo className={style.logo} />
+				</div>
+				<section className={style.heroContainer}>
+					<Image image={store.homePage.heros[this.state.activeImage]} className={cx({
+						[style.hero]: true,
+						[style.faded]: this.state.fadeHero,
+					})}/>
+				</section>
+				<section className={style.navContainer}>
+					<WidthControlled noPadding>
+						<div className={style.nav} >
+							<Link to="/blog">Blog</Link>
+							<Link to="/sponsors">Sponsors</Link>
+							<a href="mailto:upracing.fs@gmail.com">Contact</a>
+						</div>
+					</WidthControlled>
+				</section>
+			</div>
+		)
 
 		return (
 			<div className={style.container}>
@@ -75,9 +105,13 @@ class HomePage extends Component {
 		setTimeout(() => {
 			this.setState({
 				activeImage: (this.state.activeImage + 1) % this.props.store.homePage.heros.length,
+			})
+		}, 900)
+		setTimeout(() => {
+			this.setState({
 				fadeHero: false,
 			})
-		}, 400)
+		}, 950)
 	}
 }
 
