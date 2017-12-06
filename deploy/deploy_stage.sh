b1925e81130d9fb4db900ec3dab8365f2277691e
Chris Aubert <hello@chrisaubert.me>
2017-12-06 15:24:36 +0000

    Fixed deploy

--- deploy/deploy_stage.sh
+++ deploy/deploy_stage.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 gulp production
-ssh $SERVER_USER@$SERVER_IP 'rm -r /var/www/upracing.chrisaubert.me/build/'
-scp package.json $SERVER_USER@$SERVER_IP:/var/www/upracing.chrisaubert.me/
-scp -r build $SERVER_USER@$SERVER_IP:/var/www/upracing.chrisaubert.me/build/
+ssh $SERVER_USER@$SERVER_IP 'rm -r /var/www/stage.upracing.org/build/'
+scp package.json $SERVER_USER@$SERVER_IP:/var/www/stage.upracing.org/
+scp -r build $SERVER_USER@$SERVER_IP:/var/www/stage.upracing.org/build/
 
 ssh $SERVER_USER@$SERVER_IP << EOF
-    cd /var/www/chrisaubert.me
+    cd /var/www/stage.upracing.org.me
     yarn --production
-    pm2 gracefulReload upracing.chrisaubert.me
+    pm2 gracefulReload stage.upracing.org
 EOF
